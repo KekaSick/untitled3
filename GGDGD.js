7fa06d8f966f98dc59f9e0137ed4cf69db1a4e96
Допустим <mlggg@bk.ru>
2020-01-15 16:37:41 +0300

    123

---
+++ GGDGD.js
@@ -0,0 +1,21 @@
+function pow (x, n) {
+
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+
+        result *= x;
+    }
+    return result;
+}
+
+Let x = prompt("x?",'');
+Let n = prompt("n?",'');
+
+if (n <= 0) {
+    alert('Степень ${n} не поддерживается, ' +
+        'введите целую степень, большую 0');
+}
+else
+{
+    alert ( pow(x, n) );
+}
